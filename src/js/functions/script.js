ff8089542e863417d499a46554d5bd2b0d9023b1
Dimmasay <kruhliakds@gmail.com>
2023-10-10 22:45:43 +0300

    The problem with "gallery-page__title" solved, I added the function which haves two title and switch title with relative or title with position fixed

--- src/js/functions/script.js
+++ src/js/functions/script.js
@@ -1,25 +1,52 @@
 //Файл зі своїм кодом
 import {offset} from "./functions.js";
 
-
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
 }
 
+window.onload = function () {
 
+    document.addEventListener('scroll', titleStickyOrNot)
 
-document.addEventListener('scroll', (event)=>{
-    let galleryBlock = document.querySelector('.gallery-page__container')
-    let title = document.querySelector('.gallery-page__title')
+    function titleStickyOrNot() {
 
-    let blockOffset = offset(galleryBlock).top
-    let blockHeight = galleryBlock.offsetHeight
+        let galleryBlock = document.querySelector('.gallery-page__container')
+        let blockOffset = offset(galleryBlock).top
+        let blockHeight = galleryBlock.offsetHeight
 
+        let title = document.querySelector('.gallery-page__title')
+        let titleSticky = document.querySelector('.gallery-page__title_sticky')
+        titleSticky.style.position = 'fixed'
+        titleSticky.style.visibility = 'hidden'
 
+        if (window.innerWidth <= 767) {
+            titleSticky.style.top = '135px'
 
-    if(pageYOffset + window.innerHeight < blockOffset + blockHeight){
-        title.style.position = 'sticky'
-    } else {
-        title.style.position = 'static'
+            if (offset(title).top - pageYOffset < 135 && blockOffset + blockHeight > pageYOffset + window.innerHeight) {
+                title.style.visibility = 'hidden'
+                titleSticky.style.visibility = 'visible'
+            } else {
+                title.style.visibility = 'visible'
+                titleSticky.style.visibility = 'hidden'
+            }
+        }
+
+        if (window.innerWidth >= 768) {
+            titleSticky.style.top = '50px'
+            if (offset(title).top - pageYOffset < 50 && blockOffset + blockHeight > pageYOffset + window.innerHeight) {
+                title.style.visibility = 'hidden'
+                titleSticky.style.visibility = 'visible'
+            } else {
+                title.style.visibility = 'visible'
+                titleSticky.style.visibility = 'hidden'
+            }
+        }
     }
-})
+}
+
+
+
+
+
+
